496661ee8c90bb40eaae7dc4d6f968b915db072b
MedinaSilva <k.medinasilva1234@gmail.com>
2023-03-01 18:11:59 -0600

    Ejercicio sobre dividir arreglos

---
+++ promedioAnipode.js
@@ -0,0 +1,6 @@
+// Promedio antipode
+// Dado un array, devolver un array de menor longitud de acuerdo a los siguentes pasos:
+// - Dividir el array en 2 partes iguales*. Si no son iguales, eliminar el elemento en medio para obtener 2 arrays iguales
+// - Sumar cada numero de la primera parte con los numeros inversos de la segunda parte
+// [1,2,3] [5,22,6] -> 1 + 6, 2 + 22, 3 + 5 -> [7, 24, 8]
+// - Dividir cada numero del array final entre 2  -> [3.5, 12, 4]
